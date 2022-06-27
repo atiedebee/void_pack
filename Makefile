f98ba8ef15e896e5d47c129021e64c977514daa9
atiedebee <atiedebee@gmail.com>
2022-06-27 12:23:03 +0200

    made the library more official looking, added building for static and shared objects, made the packed data align properly (requires the use of C11)

---
+++ Makefile
@@ -0,0 +1,28 @@
+SRC = src/void_pack.c
+OBJ = void_pack.o
+
+CC = gcc-11
+
+FLAGS = -O2 -Wall -Wextra -Wshadow -std=c11 -Wpedantic -fpic
+FLAGS += -I include/
+
+default:
+	@printf '\n\e[1;31mInvalid usage\n\e[0;37m\n'
+	@printf '\e[1;34mmake static - create a static object file\n'
+	@printf '\e[1;34mmake shared - create a dynamically linked object file\n'
+	@printf '\e[1;34mmake clean  - remove all binaries\e[0;37m\n'
+
+.PHONY: clean static shared test
+
+static: lib
+	$(CC) $(FLAGS) $(SRC) -c -o $(OBJ)
+	ar -rcs lib/libvoid_pack.a $(OBJ)
+	rm $(OBJ)
+
+shared: lib
+	$(CC) $(FLAGS) $(SRC) -c -o $(OBJ)
+	gcc -shared $(FLAGS) $(OBJ) -o lib/libvoid_pack.so
+	rm $(OBJ)
+
+clean:
+	rm -f $(OBJ) ./a.out ./lib/*
--- src/void_pack.c
+++ src/void_pack.c
@@ -2,20 +2,45 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <stdalign.h>
+
 #include "void_pack.h"
 
 #pragma GCC diagnostic ignored "-Wpointer-arith"
 #pragma GCC diagnostic push
+
 
 void *void_unpack_var(void *buff, size_t index)
 {
 	register size_t ii;
 	size_t offset = 0;
 	char *format;
-	const size_t offset_table[256] = { 
-		#include "sizes.txt"
-	};
+	size_t sizeof_table[256] = {0};
+	size_t alignof_table[256] = {0};
+	
+	sizeof_table['c'] = sizeof(char);
+	sizeof_table['s'] = sizeof(short);
+	sizeof_table['i'] = sizeof(int);
+	sizeof_table['l'] = sizeof(long);
+	sizeof_table['p'] = sizeof(void*);
+	sizeof_table['f'] = sizeof(double);
+	sizeof_table['d'] = sizeof(double);
+	sizeof_table['D'] = sizeof(long double);
+	
+	alignof_table['c'] = alignof(char);
+	alignof_table['s'] = alignof(short);
+	alignof_table['i'] = alignof(int);
+	alignof_table['l'] = alignof(long);
+	alignof_table['p'] = alignof(void*);
+	alignof_table['f'] = alignof(double);
+	alignof_table['d'] = alignof(double);
+	alignof_table['D'] = alignof(long double);
+	
+	
+	if( buff == NULL )
+	{
+		return NULL;
+	}
 	
 	format = buff;
 	while( *(char*)buff != '\0')
@@ -28,7 +53,8 @@ void *void_unpack_var(void *buff, size_t index)
 #pragma GCC diagnostic push
 	for( ii = 0; ii < index; ii++)
 	{
-		offset += offset_table[ format[ii] ];
+		offset += sizeof_table[ format[ii] ];
+		offset += offset % alignof_table[ format[ii] ];
 	}
 	
 	return buff + offset;
@@ -44,7 +70,7 @@ void *void_pack(const char *format, ...)
 	
 	
 	format_len = strlen(format);
-	buff = malloc( (format_len * sizeof(char)) + (format_len * 8) );
+	buff = malloc( ((format_len + 1) * sizeof(char)) + (format_len * 16) );
 	if( !buff )
 	{
 		return NULL;
@@ -54,53 +80,58 @@ void *void_pack(const char *format, ...)
 	{
 		*(char*)(buff + ii) = format[ii];
 	}
-	*(char*)(buff + ii) = format[ii];
-	offset = format_len + 1;
+	*(char*)(buff + ii) = '\0';
+	offset = ii+1;
 	
-	va_start(list, format);
-	
+	/*7 different types*/
+	va_start(list, format);	
 	for( ii = 0; format[ii] != '\0'; ii++ )
 	{
 		switch( format[ii] )
 		{
 			case 'c':
+				*(char*)(buff + offset) = va_arg(list, int);
+				offset += sizeof(char);
+				offset += offset%alignof(char);
+				break;
 			case 's':
+				*(short*)(buff + offset) = va_arg(list, int);
+				offset += sizeof(short);
+				offset += offset%alignof(short);
+				break;
 			case 'i':
 				*(int*)(buff + offset) = va_arg(list, int);
 				offset += sizeof(int);
+				offset += offset%alignof(int);
 				break;
-			case 'u':
-				*(unsigned int*)(buff + offset) = va_arg(list, unsigned int);
-				offset += sizeof(unsigned int);
-				break;
-				
 			case 'l':
 				*(long*)(buff + offset) = va_arg(list, long);
 				offset += sizeof(long);
+				offset += offset%alignof(long);
 				break;
+				
 			case 'f':
 			case 'd':
 				*(double*)(buff + offset) = va_arg(list, double);
 				offset += sizeof(double);
+				offset += offset%alignof(double);
 				break;
 			case 'D':
 				*(long double*)(buff + offset) = va_arg(list, long double);
 				offset += sizeof(long double);
+				offset += offset%alignof(long double);
 				break;
 				
 			case 'p':
-			case '*':
 			default:
 				*(void**)(buff + offset) = va_arg(list, void*);
 				offset += sizeof(void*);
+				offset += offset%alignof(void*);
 				break;
 		}
 	}
+	
 	va_end(list);
-
-#ifdef VOID_PACK_MEMORY_EFFICIENCY
-	buff = realloc(buff, offset);
-#endif
 
 	return buff;
 }
@@ -127,7 +158,6 @@ int void_unpack(void *buff, ...)
 	
 	
 	va_start(list, buff);
-	
 	for( ii = 0; format[ii] != '\0'; ii++ )
 	{
 		temp = va_arg(list, void*);
@@ -135,38 +165,47 @@ int void_unpack(void *buff, ...)
 		switch( format[ii] )
 		{
 			case 'c':
+				*(char*)temp = *(char*)(buff + offset);
+				offset += sizeof(char);
+				offset += offset%alignof(char);
+				break;
 			case 's':
+				*(short*)temp = *(short*)(buff + offset);
+				offset += sizeof(short);
+				offset += offset%alignof(short);
+				break;
 			case 'i':
 				*(int*)temp = *(int*)(buff + offset);
 				offset += sizeof(int);
+				offset += offset%alignof(int);
 				break;
-			case 'u':
-				*(unsigned int*)temp = *(unsigned int*)(buff + offset);
-				offset += sizeof(unsigned int);
-				break;
-				
 			case 'l':
 				*(long*)temp = *(long*)(buff + offset);
 				offset += sizeof(long);
+				offset += offset%alignof(long);
 				break;
+				
 			case 'f':
 			case 'd':
 				*(double*)temp = *(double*)(buff + offset);
 				offset += sizeof(double);
+				offset += offset%alignof(double);
 				break;
 			case 'D':
 				*(long double*)temp = *(long double*)(buff + offset);
 				offset += sizeof(long double);
+				offset += offset%alignof(long double);
 				break;
 				
 			case 'p':
-			case '*':
 			default:
 				*(void**)temp = *(void**)(buff + offset);
 				offset += sizeof(void*);
+				offset += offset%alignof(void*);
 				break;
 		}
 	}
 	va_end(list);
 	return 0;
 }
+
